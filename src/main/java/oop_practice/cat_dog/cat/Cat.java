97b437d7a91bb089c9f26c6d145aa468a5197f20
k.hutsaliuk <hutsaliuk.k@gmail.com>
2023-10-18 21:06:56 +0300

    hw commit

--- src/main/java/oop_practice/cat_dog/cat/Cat.java
+++ src/main/java/oop_practice/cat_dog/cat/Cat.java
@@ -1,4 +1,29 @@
 package oop_practice.cat_dog.cat;
 
-public class Cat {
+import oop_practice.cat_dog.animal.Animal;
+
+public class Cat implements Animal {
+private String name;
+private String className = "Cat";
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String voice() {
+        return "Mew";
+    }
+    @Override
+    public void sound() {
+
+        System.out.println("I'am a " + className + ". My name " + getName() + ". " +  voice());
+    }
 }
--- src/main/java/oop_practice/cat_dog/dog/Dog.java
+++ src/main/java/oop_practice/cat_dog/dog/Dog.java
@@ -1,6 +1,31 @@
 package oop_practice.cat_dog.dog;
 
-public class Dog extends Animal{
+import oop_practice.cat_dog.animal.Animal;
+
+public class Dog implements Animal {
+private String name;
+private String className = "Doc";
 
 
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String voice() {
+        return "Gav";
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("I'am a " + className + ". My name is " + getName() + ". " + voice());
+    }
 }
